219bdf923d3a37d7d83ec39e2cd805c34e8dcb1d
Magnus Rand <magnus.rand@gmail.com>
2019-10-03 15:23:21 +1300

    Add random timer

--- HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/Timer.java
+++ HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/Timer.java
@@ -1,12 +1,13 @@
 package com.hci.hciresearchprojectapp;
 
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Timer extends AppCompatActivity {
 
-    public CountDownTimer createTimerTextViewInSeconds(final int seconds, final TextView textView) {
+    public CountDownTimer createTimerTextviewInSeconds(final int seconds, final TextView textView) {
         return new CountDownTimer(seconds*1000,1000) {
             int displayCounter = seconds;
             @Override
@@ -17,17 +18,39 @@ public class Timer extends AppCompatActivity {
             }
             @Override
             public void onFinish() {
-                timerDoneEvent(textView);
+                textViewTimerDoneEvent(textView);
             }
         };
     }
 
-    public void timerDoneEvent(TextView textView){
-        createTimerTextViewInSeconds(5, textView).start();
+    public CountDownTimer createRandomTimerInSeconds(final int minSeconds, final int maxSeconds) {
+        final int seconds = minSeconds + (int) (Math.random() * maxSeconds);
+        return new CountDownTimer(seconds*1000,1000) {
+            int displayCounter = seconds;
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTickUpdateEvent(displayCounter);
+                displayCounter--;
+            }
+            @Override
+            public void onFinish() {
+                randomTimerDoneEvent(minSeconds, maxSeconds);
+            }
+        };
+    }
+
+    public void textViewTimerDoneEvent(TextView textView){
+        createTimerTextviewInSeconds(5, textView).start();
+    }
+
+    public void randomTimerDoneEvent(int minSeconds, int maxSeconds) {
+        Log.d("Done", "Done");
+        createRandomTimerInSeconds(minSeconds, maxSeconds).start();
     }
 
     public void timerTickUpdateEvent(int counterValue) {
         // TODO run function that needs counterValue here
+        Log.d(String.valueOf(counterValue), String.valueOf(counterValue)); // Logs value, remove on implementation
     }
 }
 
