bf06f6a5d4df3e1772da184fb76ff52955825352
ranveersinghaiden <raid127@aucklanduni.ac.nz>
2019-10-07 12:04:36 +1300

    Added initial code for gesture detection.

---
+++ HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/GestureDetection.java
@@ -0,0 +1,87 @@
+package com.hci.hciresearchprojectapp;
+
+import android.hardware.SensorEvent;
+import android.util.Log;
+import android.view.GestureDetector;
+
+import static android.content.ContentValues.TAG;
+
+public class GestureDetection {
+
+    private float oldX = 0.0f, oldY = 0.0f, oldZ = 0.0f;
+    private int i = 0;
+    private final float delta = 1.0f;
+
+    private static final float ALPHA = 0.8f;
+    private float[] gravity = new float[3];
+    private float[] linear_acceleration = new float[3];
+
+
+
+    public String startGestureDetection(SensorEvent sensorEvent, Boolean isNotificationVisible){
+
+        String gesture = "";
+        gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[0];
+        gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * sensorEvent.values[1];
+        gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * sensorEvent.values[2];
+
+        float linearX = sensorEvent.values[0] - gravity[0];
+        float linearY = sensorEvent.values[1] - gravity[1];
+        float linearZ = sensorEvent.values[2] - gravity[2];
+
+
+        float deltaX = Math.abs(linearX - oldX);
+        float deltaY = Math.abs(linearY - oldY);
+        float deltaZ = Math.abs(linearZ - oldZ);
+
+        if(i > 0)
+        {
+            if (deltaX > deltaY) {
+                if(deltaX > delta){
+                    // Write code here for when either left or right tilt gesture is detected
+                    if (linearX < 0) {
+                        // Write code here for when phone is tilted right
+                        Log.i(TAG, "onSensorChanged: You tilt the device right\n\n\n\n\n\n\n\n");
+                    }
+                    if (linearX > 0) {
+                        // Write code here for when phone is tilted left
+                        Log.i(TAG, "onSensorChanged: You tilt the device left\n\n\n\n\n\n\n\n");
+                    }
+                    if(isNotificationVisible)
+                        gesture = "RL";
+                }
+            } else if(deltaY > delta){
+                // Write code here for when either up or down tilt gesture is detected
+                if (linearY < 0) {
+                    // Write code here for when phone is tilted up
+                    Log.i(TAG, " tilting forward : You tilt the device up\n\n\n\n\n\n\n\n");
+                }
+                if (linearY > 0) {
+                    // Write code here for when phone is tilted down
+                    Log.i(TAG, " tilting forward : You tilt the device downs\n\n\n\n\n\n\n\n");
+                }
+                if(isNotificationVisible)
+                    gesture = "UD";
+            }
+            else if (linearX > (-2) && linearX < (2) && linearY > (-2) && linearY < (2)) {
+//                    Log.i(TAG, "onSensorChanged: Not tilt device");
+                gesture = "NONE";
+            }
+            else
+                gesture = "SHAKE";
+        }
+
+        oldX = linearX;
+        oldY = linearY;
+
+        i++;
+
+
+        try {
+            Thread.sleep(150);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return gesture;
+    }
+}
--- HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/MainActivity.java
+++ HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/MainActivity.java
@@ -2,16 +2,31 @@ package com.hci.hciresearchprojectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sm;
+    private GestureDetection gDetector = new GestureDetection();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListener , sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
         Button btn1 = findViewById(R.id.startTrainingBtn);
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -23,4 +38,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Create the gesture listener
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            gDetector.startGestureDetection(sensorEvent, isNotificationVisible);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 }
