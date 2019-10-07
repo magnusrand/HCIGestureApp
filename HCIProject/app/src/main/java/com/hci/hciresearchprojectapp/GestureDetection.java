3d897f29a591d71eb60d6b1191789e09913e7d8d
ranveersinghaiden <raid127@aucklanduni.ac.nz>
2019-10-07 20:37:15 +1300

    changed gesture recognition method to accept the assigned gesture and return true if assigned gesture is performed.

--- HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/GestureDetection.java
+++ HCIProject/app/src/main/java/com/hci/hciresearchprojectapp/GestureDetection.java
@@ -2,7 +2,6 @@ package com.hci.hciresearchprojectapp;
 
 import android.hardware.SensorEvent;
 import android.util.Log;
-
 import static android.content.ContentValues.TAG;
 
 public class GestureDetection {
@@ -28,53 +27,50 @@ public class GestureDetection {
         isNotificationDisplayed = flag;
     }
 
-    public String startGestureDetection(SensorEvent sensorEvent){
-        String gesture = "";
-        if(isNotificationDisplayed) {
+    public Boolean startGestureDetection(SensorEvent sensorEvent, int assignedGesture){
+        float linearX = 0.0f, linearY = 0.0f, linearZ = 0.0f, deltaX = 0.0f, deltaY = 0.0f,deltaZ = 0.0f;
 
-            float linearX = 0.0f, linearY = 0.0f, linearZ = 0.0f, deltaX = 0.0f, deltaY = 0.0f,deltaZ = 0.0f;
+        gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[0];
+        gravity[1] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[1];
+        gravity[2] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[2];
 
-            gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[0];
-            gravity[1] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[1];
-            gravity[2] = ALPHA * gravity[0] + (1 - ALPHA) * sensorEvent.values[2];
+        linearX = sensorEvent.values[0] - gravity[0];
+        linearY = sensorEvent.values[1] - gravity[1];
+        linearZ = sensorEvent.values[2] - gravity[2];
 
-            linearX = sensorEvent.values[0] - gravity[0];
-            linearY = sensorEvent.values[1] - gravity[1];
-            linearZ = sensorEvent.values[2] - gravity[2];
+        deltaX = Math.abs(linearX - oldX);
+        deltaY = Math.abs(linearY - oldY);
+        deltaZ = Math.abs(linearZ - oldZ);
 
-            deltaX = Math.abs(linearX - oldX);
-            deltaY = Math.abs(linearY - oldY);
-            deltaZ = Math.abs(linearZ - oldZ);
+        oldX = linearX;
+        oldY = linearY;
+        oldZ = linearZ;
 
-            oldX = linearX;
-            oldY = linearY;
-            oldZ = linearZ;
+        Log.i(TAG, "startGestureDetection: linear velocities are " + deltaX + " " + deltaY + " " + deltaZ);
+        if (deltaX > deltaY) {
+            if((deltaX > tiltDelta) && (assignedGesture == 2))
+                return true;
 
-            if (deltaX > deltaY) {
-                if(deltaX > tiltDelta){
-                    gesture = "RL";
-                }
-            } else if(deltaY > tiltDelta){
-                gesture = "UD";
-            }
+        }
+        else if((deltaY > tiltDelta) && (assignedGesture == 1))
+            return true;
 //            else if (linearX > (-2) && linearX < (2) && linearY > (-2) && linearY < (2)) {
 //                gesture = "NONE";
 //            }
-            if(deltaZ > shakeDetla || deltaX > shakeDetla || deltaY > shakeDetla) {
-                gesture = "SHAKE";
-            }
-
-            oldX = linearX;
-            oldY = linearY;
-
-            try {
-                Thread.sleep(150);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if((deltaZ > shakeDetla || deltaX > shakeDetla || deltaY > shakeDetla) && (assignedGesture == 3)) {
+            return true;
         }
-        Log.i(TAG, "gesture is " + gesture);
-        return gesture;
+
+        oldX = linearX;
+        oldY = linearY;
+
+        try {
+            Thread.sleep(150);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
 
